b55e74a272124518bc697c73ea99623cae77cc64
Monika Aki <monika.aki@proton.me>
2026-03-13 01:30:15 +0100

    Add widget preview

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.akinom.isod"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.2.2"
+        versionCode = 30
+        versionName = "0.3"
     }
 
     packaging {
